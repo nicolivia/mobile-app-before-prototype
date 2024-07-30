92ecae3c10984172fbba6f38312091dc906886a7
whereishikaru <h.suzuki.028@gmail.com>
2024-07-30 22:50:15 +1200

    feat: created necessary modules for login in backend

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from config import ApplicationConfig
+from flask_session import Session
+from flask_migrate import Migrate
+from models import db
+from config import ApplicationConfig, MailConfig
 
 app = Flask(__name__)
-app.config.from_object(ApplicationConfig)
-CORS(app, supports_credentials=True)
+app.config.from_object(ApplicationConfig, MailConfig)
+db.init_app(app)
+CORS(app, supports_credentials=True, origins=['http://localhost:3000'])
+
+# Initialise Redis for session management
+Session(app)
+
+migrate = Migrate(app, db)
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+# Import routes
+import routes
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
---
+++ backend/app/models.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# Customers' database schema
+class Customers(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.String(32), primary_key=True, unique=True)
+    full_name = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(345), unique=True, nullable=True)
+    phone = db.Column(db.String(20), unique=True, nullable=True)
+    bod = db.Column(db.Date, nullable=True)
+    ethnicity = db.Column(db.String(20), nullable=True)
+    gender = db.Column(db.String(10), nullable=True)
+    address = db.Column(db.String(120), nullable=True)
+    temporary_password = db.Column(db.String(6), nullable=True)
+    password_expiry = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'fullName': self.full_name,
+            'email': self.email,
+            'phone': self.phone,
+            'bod': self.bod,
+            'ethnicity': self.ethnicity,
+            'gender': self.gender,
+            'address': self.address,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
+        }
+
+# Employees' database schema    
+class Employees(db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.String(32), primary_key=True, unique=True)
+    employee_id = db.Column(db.String(10), unique=True, nullable=False)
+    full_name = db.Column(db.String(80), unique=True, nullable=False)
+    img_url = db.Column(db.String(200), nullable=True)
+    temporary_password = db.Column(db.String(6), nullable=True)
+    password_expiry = db.Column(db.DateTime, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'employeeId': self.employee_id,
+            'fullName': self.full_name,
+            'imgUrl': self.img_url,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at
+        }
+    
