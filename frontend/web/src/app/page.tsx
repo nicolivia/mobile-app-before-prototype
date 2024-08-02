e1cdd3aa86eb73ba863648fdffac91a2a886cc7e
nicolivia <763131480@qq.com>
2024-08-02 16:11:26 +1200

    Login_page created

---
+++ frontend/web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from '../components/LoginPage';
+
+export default function Home() {
+  return (
+    <main className="w-full h-min-screen">
+      <LoginPage />
+    </main>
+  );
+}
