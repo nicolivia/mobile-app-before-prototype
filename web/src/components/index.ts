c74c841234ef8fff5679550dc10168ec634e7fb2
whereishikaru <h.suzuki.028@gmail.com>
2024-08-06 10:50:32 +1200

    Deleted unnecessary files

--- web/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import ThemeProvider from './themes/ThemeProvider'
-import ThemeToggler from './themes/ThemeToggler'
-import LanguageOption from './LanguageOption'
-
-export {
-    ThemeProvider,
-    ThemeToggler,
-    LanguageOption,
-}
